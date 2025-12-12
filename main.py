223eb4d7dfb8a7d84f8a0bdd8a1e26e551d2b9c4
BogdanZ19 <bogdanzainia1@gmail.com>
2025-12-12 19:40:17 +0200

    add --verbose argument and prepare to store messages

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
+
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-    model = "gemini-2.5-flash"
-    
+
     if api_key is None:
         raise RuntimeError("API KEY NOT FOUND")
-    
-    # Get the prompt from the user
+
+    # Get the prompt and arguments from the user
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    
+
+    # Prepare to store conversations
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     # Create the client and make a call with the provided prompt
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model=model, contents=args.user_prompt);
-    
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=messages
+    )
+
     # Get token counts
+    prompt_tokens_count = 0
+    response_tokens_count = 0
     if response.usage_metadata is not None:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}\n")
+        prompt_tokens_count = response.usage_metadata.prompt_token_count
+        response_tokens_count = response.usage_metadata.candidates_token_count
     else:
         raise RuntimeError("Usage Metadata Not Available")
-    
+
+    # Console output:
+    if args.verbose is True:
+        print(f"\tUser prompt: {args.user_prompt}")
+        print(f"\tPrompt tokens: {prompt_tokens_count}")
+        print(f"\tResponse tokens: {response_tokens_count}\n")
+
+    print("Response:")
     print(response.text)
 
 
